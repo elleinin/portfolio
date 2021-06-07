641f18dd0e3d8c963f31e4fd72b63a9e8dc09e34
Ninielle <nga.pascual@gmail.com>
2021-06-07 21:12:50 +0800

    responsiveness testing interactive media page

--- dist/js/spec.js
+++ dist/js/spec.js
@@ -5,7 +5,9 @@ const gd = document.querySelector('.gd-card');
 const sa = document.querySelector('.sa-card');
 const imTv = document.querySelector('.im-tv');
 const eks = document.querySelector('.tab');
-const em = document.querySelector('im-desc-icons');
+const imWeb = document.querySelector('.im-desc-web');
+const imGd = document.querySelector('.im-desc-gd');
+// const em = document.querySelector('im-desc-icons');
 const a = document.querySelector('.a');
 const b = document.querySelector('.b');
 
@@ -13,6 +15,7 @@ const b = document.querySelector('.b');
 im.addEventListener('click', toggleIm);
 eks.addEventListener('click', toggleClose);
 a.addEventListener('click', toggleWeb);
+b.addEventListener('click', toggleGd);
 
 // let abtP = true;
 let imP = false;
@@ -38,7 +41,7 @@ function toggleClose() {
     abt.classList.add('show');
     imPage.classList.remove('show');
     imTv.classList.remove('show');
-    imP = false;
+    imP = true;
     web = false;
     game = false;
 
@@ -48,16 +51,47 @@ function toggleClose() {
 function toggleWeb() {
     console.log('web');
     if (imP) {
-        em.classList.remove('show');
-        a.classList.add('show');
+        // em.classList.remove('show');
+        imWeb.classList.add('show');
         imP = false;
         web = true;
+        console.log('web:' + web);
+        console.log('imP:' + imP);
     } else if (game) {
-        b.classList.remove('show');
-        a.classList.add('show');
+        imGd.classList.remove('show');
+        imWeb.classList.add('show');
         game = false;
         web = true;
     } else {
-        web = true;
+        imWeb.classList.remove('show');
+        web = false;
+        imP = true;
     }
+    console.log('web:' + web);
+    console.log('gd:' + game);
+    console.log('imP:' + imP);
+}
+
+function toggleGd() {
+    console.log('game design');
+    if (imP) {
+        // em.classList.remove('show');
+        imGd.classList.add('show');
+        imP = false;
+        game = true;
+        console.log('web:' + web);
+        console.log('imP:' + imP);
+    } else if (web) {
+        imWeb.classList.remove('show');
+        imGd.classList.add('show');
+        game = true;
+        web = false;
+    } else {
+        imGd.classList.remove('show');
+        game = false;
+        imP = true;
+    }
+    console.log('web:' + web);
+    console.log('gd:' + game);
+    console.log('imP:' + imP);
 }
