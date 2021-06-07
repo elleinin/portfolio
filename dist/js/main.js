198e0b22ba7b1af5297100cc6724b5192170fd78
Ninielle <nga.pascual@gmail.com>
2021-06-07 16:01:37 +0800

    responsiveness testing two

--- dist/js/main.js
+++ dist/js/main.js
@@ -16,6 +16,7 @@ const nameHeader = document.querySelector('.card-title');
 const specHeader = document.querySelector('.card-subtitle');
 const textHeader = document.querySelector('.card-text');
 const lineHeader = document.querySelector('.card-line');
+// const flipCard = document.querySelector('.card-nav');
 //querySelectorAll selects all and puts it into a Node (like an array)
 const icons = document.querySelectorAll('.card-icons');
 const navItems = document.querySelectorAll('.nav-item');
@@ -25,7 +26,7 @@ const navItems = document.querySelectorAll('.nav-item');
 //MENU
 // let showMenu = false; //let
 //BUSINESSCARD
-let flipCard = false;
+let flip = false;
 //SET INITIAL STATES--------------------------------------------------
 
 //SET EVENT LISTENERS-------------------------------------------------
@@ -35,7 +36,7 @@ businessCard.addEventListener('click', toggleNavi);
 
 //BUSINESS CARD NAVI--------------------------------------------------
 function toggleNavi() {
-    if (!flipCard) {
+    if (!flip) {
         businessCard.classList.add('flip'); //businessCard.style.transform = 'rotateY(180deg)';
         //businessCard.style.animation = 'rotateY(180deg)';
         nameHeader.classList.add('hidden');
@@ -46,7 +47,7 @@ function toggleNavi() {
         navItems.forEach(item => item.classList.add('show'));
 
         //set card state
-        flipCard = true;
+        flip = true;
     } else {
         businessCard.classList.remove('flip');
         nameHeader.classList.remove('hidden');
@@ -57,7 +58,7 @@ function toggleNavi() {
         navItems.forEach(item => item.classList.remove('show'));
 
         //set card state
-        flipCard = false;
+        flip = false;
     }
 }
 
